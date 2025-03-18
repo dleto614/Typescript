e34a1605c809bed703a45f70698295b70b07b0f5
dleto614 <dlebeau.03@protonmail.com>
2025-03-18 02:32:34 -0400

    Update enter-name.ts

--- enter-name.ts
+++ enter-name.ts
@@ -3,7 +3,7 @@ import { stdin as input, stdout as output } from 'process';
 
 const rl = readline.createInterface({ input, output });
   
-async function GetName() {
+async function GetName(): Promise<string> {
     const answer = await rl.question('Who are you? ');
     
     return answer;
@@ -12,7 +12,7 @@ async function GetName() {
 
 async function main(): Promise<void> {
 
-        var name = await GetName();
+        const name = await GetName();
 
         if ( name.length != 0 ) {
             console.log(`Hey there, ${name}!`);
@@ -27,4 +27,3 @@ async function main(): Promise<void> {
 (async () => {
     await main().catch(console.error);
 })();
-   
